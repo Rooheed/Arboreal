1b3759d208ca552fea29d8daa82912d9b5636786
rooheed <rooheed.gulistani@gmail.com>
2017-12-07 18:35:44 +0100

    First website upload

---
+++ resources/js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    /* Scroll on button */
+    $('.js--scroll-to-themes').click(function(){
+    	$('html, body').animate({scrollTop: $('.js--section-themes').offset().top}, 1000);
+    });
+    
+    $('.js--scroll-to-plans').click(function(){
+    	$('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
+
+    /* Navigation scroll */
+
+    $(function() {
+      $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+        });
+      });
+});
+  
